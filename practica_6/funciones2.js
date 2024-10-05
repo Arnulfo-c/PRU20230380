ba805aa977babba8e09116fa38a842aaf77753ff
Arnulfo cruz <arnulfocrz2783@gmail.com>
2024-10-05 15:57:45 -0600

    commit

---
+++ practica_6/funciones2.js
@@ -0,0 +1,7 @@
+console.log("Menu opciones");
+console.log("1.Mostrar el numero mayor de un arreglo");
+console.log("2.Imprimir el total de vocales y mostrar cuales son de una cadena\n");
+
+let opcion = 1;
+
+switch (opcion)
